e698e00b14690711f7e76b45892e0ebcb26e0d83
Maksym <maksim.semenchenko@techfabric.com>
2022-10-13 19:01:52 +0300

    Added projection attributes to EntityInstanceProjectionDocument

--- CloudFabric.EAV/Projections/EntityInstanceProjection/EntityInstanceProjectionDocument.cs
+++ CloudFabric.EAV/Projections/EntityInstanceProjection/EntityInstanceProjectionDocument.cs
@@ -2,12 +2,16 @@ using CloudFabric.EAV.Domain.Models;
 using System.Collections.Generic;
 using System;
 using CloudFabric.Projections;
+using CloudFabric.Projections.Attributes;
 
 namespace CloudFabric.EAV.Domain.Projections.EntityInstanceProjection;
 
+[ProjectionDocument]
 public class EntityInstanceProjectionDocument : ProjectionDocument
 {
+    [ProjectionDocumentProperty(IsFilterable = true)]
     public Guid EntityConfigurationId { get; set; }
 
+    [ProjectionDocumentProperty(IsNested = true)]
     public List<AttributeInstance> Attributes { get; set; }
 }
