1964995619166ed57559b760fb7c01f4eb5b37e4
Maksym <maksim.semenchenko@techfabric.com>
2022-11-25 17:58:51 +0200

    Added Id to AttributeConfigurationListItemViewModel and GetAttribute method

--- CloudFabric.EAV.Models/ViewModels/AttributeConfigurationListItemViewModel.cs
+++ CloudFabric.EAV.Models/ViewModels/AttributeConfigurationListItemViewModel.cs
@@ -2,6 +2,8 @@ namespace CloudFabric.EAV.Models.ViewModels;
 
 public class AttributeConfigurationListItemViewModel
 {
+    public Guid? Id { get; set; }
+    
     public List<LocalizedStringViewModel> Name { get; set; }
 
     public string MachineName { get; set; }
