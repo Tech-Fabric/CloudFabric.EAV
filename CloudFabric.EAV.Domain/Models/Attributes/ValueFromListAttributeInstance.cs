db8ca8cc716d85b9f962d5956af099b5c330e0a5
Dmytro Honcharenko <dmytro.honcharenko@techfabric.com>
2022-12-14 14:35:50 +0200

    Added preselected options

--- CloudFabric.EAV.Domain/Models/Attributes/ValueFromListAttributeInstance.cs
+++ CloudFabric.EAV.Domain/Models/Attributes/ValueFromListAttributeInstance.cs
@@ -3,5 +3,6 @@ namespace CloudFabric.EAV.Domain.Models.Attributes
     public class ValueFromListAttributeInstance : AttributeInstance
     {
         public List<string> UnavailableOptionsMachineNames { get; set; }
+        public List<string> PreselectedOptionsMachineNames { get; set; }
     }
 }
