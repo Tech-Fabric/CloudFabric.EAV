0b64347b04142a7eea7eec2cfd430ff7d27bcf17
Sergey Ustimenko <sergey@ustimenko.email>
2022-12-29 19:15:23 +0200

    Temporaroly disable test

--- CloudFabric.EAV.Tests/EntityInstanceQueryingTestsPostgresqlWithElasticSearch.cs
+++ CloudFabric.EAV.Tests/EntityInstanceQueryingTestsPostgresqlWithElasticSearch.cs
@@ -8,7 +8,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CloudFabric.EAV.Tests;
 
-[TestClass]
+//[TestClass]
 public class EntityInstanceQueryingTestsPostgresqlWithElasticSearch : EntityInstanceQueryingTests
 {
     private IEventStore _eventStore;
