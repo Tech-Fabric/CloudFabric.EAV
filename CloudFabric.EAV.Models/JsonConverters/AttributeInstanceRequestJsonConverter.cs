3964395b3695c2f0c5e582d5b3969d2b7a811f09
Maksym <maksim.semenchenko@techfabric.com>
2023-01-25 13:03:26 +0200

    Added AttributeInstanceRequestJsonConverter

---
+++ CloudFabric.EAV.Models/JsonConverters/AttributeInstanceRequestJsonConverter.cs
@@ -0,0 +1,65 @@
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+using CloudFabric.EAV.Domain.Enums;
+using CloudFabric.EAV.Models.RequestModels.Attributes;
+
+namespace CloudFabric.EAV.Models.JsonConverters
+{
+    public class AttributeInstanceRequestJsonConverter<T> : JsonConverter<T>
+    {
+        public override bool CanConvert(Type type)
+        {
+            return typeof(T).IsAssignableFrom(type);
+        }
+
+        public override T Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var obj = JsonNode.Parse(ref reader);
+
+            if (obj == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var attributeTypeName = Enum.Parse<EavAttributeType>(obj["valueType"]?.ToString() ?? throw new InvalidOperationException());
+            Type type = GetRequestTypeFromAttributeType(attributeTypeName);
+
+            var updatedOptions = new JsonSerializerOptions(options);
+            updatedOptions.PropertyNameCaseInsensitive = true;
+            updatedOptions.Converters.Add(new JsonStringEnumConverter(updatedOptions.PropertyNamingPolicy));
+
+            var attributeRequestModel = JsonSerializer.Deserialize(obj.ToString(), type, updatedOptions);
+            return (T)attributeRequestModel! ?? throw new InvalidOperationException();
+        }
+
+        private static Type GetRequestTypeFromAttributeType(EavAttributeType attributeTypeName)
+        {
+            switch (attributeTypeName)
+            {
+                case EavAttributeType.Number:
+                    return typeof(NumberAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.LocalizedText:
+                    return typeof(LocalizedTextAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.Text:
+                    return typeof(TextAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.DateRange:
+                    return typeof(DateRangeAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.Boolean:
+                    return typeof(BooleanAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.File:
+                    return typeof(FileAttributeInstanceCreateUpdateRequest);
+                case EavAttributeType.ValueFromList:
+                    return typeof(ValueFromListAttributeInstanceCreateUpdateRequest);
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CloudFabric.EAV.Models/RequestModels/AttributeInstanceCreateUpdateRequest.cs
+++ CloudFabric.EAV.Models/RequestModels/AttributeInstanceCreateUpdateRequest.cs
@@ -1,7 +1,16 @@
+using System.Text.Json.Serialization;
+
+using CloudFabric.EAV.Domain.Enums;
+using CloudFabric.EAV.Models.JsonConverters;
+
 namespace CloudFabric.EAV.Models.RequestModels.Attributes
 {
+    [JsonConverter(typeof(AttributeInstanceRequestJsonConverter<AttributeInstanceCreateUpdateRequest>))]
     public abstract class AttributeInstanceCreateUpdateRequest
     {
         public string ConfigurationAttributeMachineName { get; set; }
+
+        // Needed for deserialization
+        public EavAttributeType ValueType { get; set; }
     }
 }
