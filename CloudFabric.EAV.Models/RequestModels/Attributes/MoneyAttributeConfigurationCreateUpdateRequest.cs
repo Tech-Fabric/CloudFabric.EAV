48ec25f272379371079e403dd8f354347779f234
Dmytro Honcharenko <dmytro.honcharenko@techfabrc.com>
2023-04-25 14:59:25 +0300

    Make money config options optional

--- CloudFabric.EAV.Models/RequestModels/Attributes/MoneyAttributeConfigurationCreateUpdateRequest.cs
+++ CloudFabric.EAV.Models/RequestModels/Attributes/MoneyAttributeConfigurationCreateUpdateRequest.cs
@@ -5,6 +5,6 @@ namespace CloudFabric.EAV.Models.RequestModels.Attributes;
 public class MoneyAttributeConfigurationCreateUpdateRequest: AttributeConfigurationCreateUpdateRequest
 {
     public override EavAttributeType ValueType => EavAttributeType.Money;
-    public List<CurrencyRequestModel> Currencies { get; set; }
-    public string DefaultCurrencyId { get; set; }
+    public List<CurrencyRequestModel>? Currencies { get; set; }
+    public string? DefaultCurrencyId { get; set; }
 }
