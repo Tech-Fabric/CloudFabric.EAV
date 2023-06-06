738f64ad611ca538e7d4c4e6f08484b3be8c7c33
Dmitriy Melnychenko <dmitriy.melnichenko@techfabric.com>
2023-06-06 18:29:40 +0300

    Added comment for MaxSize

--- CloudFabric.EAV.Options/ElasticSearchQueryOptions.cs
+++ CloudFabric.EAV.Options/ElasticSearchQueryOptions.cs
@@ -1,5 +1,8 @@
 namespace CloudFabric.EAV.Options;
 public class ElasticSearchQueryOptions
 {
+    // Supported max count of returned records.
+    // If you have to return more than 10 000 records,
+    // take care of implementing SearchAfter() in ElasticSearchProjectionRepository
     public int MaxSize { get; set; } = 10000;
 }
